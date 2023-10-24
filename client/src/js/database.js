1f672510432707ddae4c1998e7dab8a5e4aeb466
nfillip <nick.m.fillip@gmail.com>
2023-10-23 20:11:47 -0600

    finished service work js and database.js, still need to bug fix

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// export const putDb = async (content) => console.error('putDb not implemented');
+
+export const putDb = async (content) => {
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({value: content});
+  const result = await request;
+  console.log('New item saved to database:', result.value)
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log(result)
+  //if result is truthy return .value
+  return result?.value; 
+};
 
 initdb();
